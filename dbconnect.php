738ba7816ae62fc21bb26756c22eba855d5548a4
NorihisaOmori <nonchi842@gmail.com>
2018-03-14 11:43:04 +0800

    db接続操作をまとめた

---
+++ dbconnect.php
@@ -0,0 +1,21 @@
+
+<?php
+// １．データベースに接続する
+  $dsn = 'mysql:dbname=LAA0943760-nolis328;host=mysql108.phy.lolipop.lan';//コロンは「使いますよ」の意味,ローカルホストは自分のサーバーという意味別の場合はIP
+  $user = 'LAA0943760';
+  $password='piruro328';
+  $dbh = new PDO($dsn, $user, $password);
+  $dbh->query('SET NAMES utf8');
+          //dbに何を入れるか？送信情報＋送信識別子＋カラム数
+   ?>
+
+<!--
+<?php
+	// $dsn = 'mysql:dbname=oneline_bbs;host=localhost';//コロンは「使いますよ」の意味,ローカルホストは自分のサーバーという意味別の場合はIP
+	// $user = 'root';
+	// $password='';
+	// $dbh = new PDO($dsn, $user, $password);
+	// $dbh->query('SET NAMES utf8');
+?>
+
+-->
